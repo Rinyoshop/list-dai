21bb84e42268d640741a62ba31b7c9b889acfc05
Rinyoshop <jumphon@gmail.com>
2025-06-12 09:22:22 +0700

    first commit

--- ccdp.py
+++ ccdp.py
@@ -1,1 +1,11 @@
-print = "hello word"
+# hello.py
+
+# Print a welcome message
+print("Hello, World!")
+
+# Ask for the user's name
+name = input("What's your name? ")
+
+# Greet the user
+print(f"Nice to meet you, {name}!")
+
